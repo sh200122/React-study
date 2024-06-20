0f6ccf80dad03405c195e7c64e8dc235266f4202
ahao <ahao20010222@gmail.com>
2024-06-20 15:22:49 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  const subject='React'
+  console.log(props);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hello
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          {subject}
+        </p>
       </header>
     </div>
   );
